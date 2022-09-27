0ed2feada976d66e91702514b9cc844e216e418e
Mustafa Mert Kurdoglu <mustafamert@gmail.com>
2022-09-27 13:36:10 +0300

    Sıralama programı hesaplandı

---
+++ Siralama.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Siralama {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("1. Sayı :");
+        a = inp.nextInt();
+        System.out.print("2. Sayı :");
+        b = inp.nextInt();
+        System.out.print("3. Sayı :");
+        c = inp.nextInt();
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.print("a<b<c");
+            } else {
+                System.out.print("a<c<b");
+            }
+        } else if ((b < a )&&( b < c)) {
+            if (a < c) {
+                System.out.print("b<a<c");
+            } else {
+                System.out.print("b<c<a");
+            }
+        } else if (a < b) {
+                System.out.print("c<a<b");
+            } else {
+                System.out.print("c<b<a");
+            }
+        }
+    }
+
